f8f09e5ccea263f1de102c4263b654356afc4579
cgccld <tasibii.web3@gmail.com>
2024-07-26 15:55:29 +0700

    ✏️ add guide dos

--- src/page/Dos.js
+++ src/page/Dos.js
@@ -6,12 +6,18 @@ const Dos = () => {
   return (
     <Space className="container dos" direction="horizontal" style={{width: '100%', justifyContent: 'center'}}>
       <iframe
-        src="https://kitao.github.io/pyxel/wasm/launcher/?run=cgccld.dos.main"
+        src="https://kitao.github.io/pyxel/wasm/launcher/?run=cgccld.dos.main&gamepad=enabled&packages=numpy,pandas"
         title="Dos"
         width="640px"
         height="600px"
         style={{ border: 'none' }}
       ></iframe>
+      <div>
+        <h4>[◀] to left</h4>
+        <h4>[▶] to right</h4>
+        <h4>[🅿] P to pause</h4>
+        <h4>[🔄] R to restart</h4>
+      </div>
     </Space>
   );
 } 
