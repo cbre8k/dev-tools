2581aec9c4f909bd959e9a98c1632dabae307474
cgccld <tasibii.web3@gmail.com>
2024-07-26 16:35:58 +0700

    📱fix responsive

--- src/page/Dos.js
+++ src/page/Dos.js
@@ -5,14 +5,13 @@ import { Space } from 'antd';
 const Dos = () => {
   return (
     <Space className="container dos" direction="horizontal" style={{width: '100%', justifyContent: 'center'}}>
-      <iframe
-        src="https://kitao.github.io/pyxel/wasm/launcher/?run=cgccld.dos.main&gamepad=enabled&packages=numpy,pandas"
-        title="Dos"
-        width="640px"
-        height="600px"
-        style={{ border: 'none' }}
-      ></iframe>
-      <div>
+      <div className="iframe-container">
+        <iframe
+          src="https://kitao.github.io/pyxel/wasm/launcher/?run=cgccld.dos.main&gamepad=enabled&packages=numpy,pandas"
+          title="Dos"
+        ></iframe>
+      </div>
+      <div className="instructions">
         <h4>[◀] to left</h4>
         <h4>[▶] to right</h4>
         <h4>[🅿] P to pause</h4>
